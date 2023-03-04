1af2cb4fb8a1a8341aa9bfedd65cc02ed390e033
nabilesall <nabilesall@gmail.com>
2023-03-04 22:55:32 +0100

    request

--- app/src/main/java/com/blablapp/blablapp/ChatActivity.kt
+++ app/src/main/java/com/blablapp/blablapp/ChatActivity.kt
@@ -2,47 +2,24 @@ package com.blablapp.blablapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.chat_activity.*
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.time.Duration.Companion.milliseconds
 
 class ChatActivity : AppCompatActivity() {
 
     private var userName : String = ""
-    private var linkImage : String = ""
-    private var idForum : Int = -1
-    private lateinit var messageAdapter: MessageAdapter
-    private lateinit var listOfMessage: ArrayList<UserMessage>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chat_activity)
 
-
-
         getMessage()
-
-
-        listOfMessage = ArrayList()
-        messageAdapter = MessageAdapter(this, listOfMessage)
-
-        messageRecyclerView.layoutManager = LinearLayoutManager(this)
-        messageRecyclerView.adapter = messageAdapter
-
-        userName = intent?.extras?.getString("user").toString()
-        linkImage = intent?.extras?.getString("linkImage").toString()
-        idForum = intent?.extras?.getInt("idForum").toString().toInt()
 
         sendMsg.setOnClickListener{
             if (userTexForMsg.text?.isNotEmpty()!!) {
                 sendMessage()
                 val msg = userTexForMsg.text.toString()
-                listOfMessage.add(UserMessage(idForum, 1 , userName, linkImage, msg))
 
-                // Scroll to the bottom of the list and show the new message
-                messageRecyclerView.smoothScrollToPosition(messageAdapter.itemCount - 1)
-                messageAdapter.notifyDataSetChanged()
+                userName = intent?.extras?.getString("user").toString()
+                MessageCustom(this, userName, msg, layout)
 
                 userTexForMsg.text!!.clear()
             }
@@ -56,9 +33,9 @@ class ChatActivity : AppCompatActivity() {
 
                 for (message in messages) {
                     runOnUiThread {
-                        //MessageCustom(this, message.nickname, message.messageContent, layout)
+                        MessageCustom(this, message.nickname, message.messageContent, layout)
                     }
-                    
+
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
