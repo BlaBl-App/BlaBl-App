d270f5999ecfaae4e4d7b5cf487a14b8ccab58b5
icepick4 <remi.jara@laposte.net>
2023-03-04 12:02:17 +0100

    adding texts + refractor variables + navigate to github on image click

--- app/src/main/java/com/blablapp/blablapp/AboutUsActivity.kt
+++ app/src/main/java/com/blablapp/blablapp/AboutUsActivity.kt
@@ -1,10 +1,14 @@
 package com.blablapp.blablapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.aboutus_activity.*
 import kotlinx.android.synthetic.main.setup_profil_activity.*
+import kotlinx.android.synthetic.main.setup_profil_activity.bottomNavigationView
+import java.net.URL
 
 @Suppress("DEPRECATION")
 class AboutUsActivity : AppCompatActivity() {
@@ -16,6 +20,21 @@ class AboutUsActivity : AppCompatActivity() {
         bottomNavigationView.selectedItemId = R.id.about_us
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener)
 
+        imageViewIcePick4.setOnClickListener {
+            navigationToGitHub("icepick4")
+        }
+        imageViewNabileSall.setOnClickListener {
+            navigationToGitHub("nabilesall")
+        }
+        imageViewQypol342.setOnClickListener{
+            navigationToGitHub("qypol342")
+        }
+    }
+
+    private fun navigationToGitHub(id: String){
+        val url = URL("https://github.com/".plus(id))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url.toString()))
+        startActivity(intent)
     }
 
     private val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
